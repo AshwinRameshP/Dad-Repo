6904b74529d9a713334477dd9774886e67a5b3e6
Ashwin <ashwinrameshp@gmail.com>
2024-07-15 20:28:12 +0530

    Adding quantity check function

--- PurchaseOrder.py
+++ PurchaseOrder.py
@@ -7,19 +7,28 @@ RATHISH MOTORS
 #Packages
 import pandas as pd
 
-def pre_process(dataFrame):
+def filter_PartNO(dataFrame):
     updated_dataFrame = dataFrame
-    updated_dataFrame['PART NO'] = updated_dataFrame['PART NO'].str.split().str[0]
+    updated_dataFrame['PART NO'] = updated_dataFrame['PART NO'].str.split(r'\*|\s',expand=False).str[0]
     return updated_dataFrame
+
+def QuantityCheck(dataframe):
+    checkParts=dataframe[dataframe['Quantity'] in range(1,10) ]
+    #checkParts=dataframe[dataframe['QTY']<0]
+    print(checkParts)
+    return 
 
 if __name__ == "__main__":
     #Read Excel file
-    df = pd.read_excel('PUR-ORDER-RATHISH-NILESH-23.12.2022.xlsx')
-    print(df)
+    df = pd.read_excel('PUR-ORDER-RATHISH-RISHABH-15.07.2024.xlsx')
+    #print(df)
     #Pre-Processing
-    updated_df = pre_process(df)
-    print(updated_df)
+    updated_df = filter_PartNO(df)
+    #print(updated_df)
+    #QuantityCheck(updated_df)
     updated_df.to_excel("output.xlsx",index=False)
         
 
 
+
+  
