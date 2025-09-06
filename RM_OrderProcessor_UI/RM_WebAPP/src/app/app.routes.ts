2d172aea647fdb1364d86b09ab1162236b534b93
Ashwin <ashwinrameshp@gmail.com>
2025-09-06 19:38:54 +0530

    Adding new Uploader componenet and moving existing s3 uploader to ./s3 path

--- RM_OrderProcessor_UI/RM_WebAPP/src/app/app.routes.ts
+++ RM_OrderProcessor_UI/RM_WebAPP/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { S3UploaderComponent } from './file-upload/file-upload';
+import { Uploader } from './uploader/uploader';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Uploader },
+  { path: 's3', component: S3UploaderComponent}
+];
